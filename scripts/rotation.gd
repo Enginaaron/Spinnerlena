6e84ae63e1f580adf651a447ec6bfa2750933bb7
Enginaaron <165845646+Enginaaron@users.noreply.github.com>
2025-03-15 21:59:36 -0400

    pretty much done

--- scripts/rotation.gd
+++ scripts/rotation.gd
@@ -2,10 +2,16 @@ extends Node3D
 
 @export var rotation_speed: float = 0.0  # Initial speed (RPM)
 var rotation_per_frame: float = 0.0
+const decelerate: float = 5.0
 
 func _process(delta):
-	rotation_per_frame = (rotation_speed / 60.0) * TAU * delta  # Convert RPM to radians per frame
+	rotation_per_frame = (rotation_speed / 60.0) * TAU * delta  # convert RPM to radians per frame
 	rotate_y(rotation_per_frame)
+	rotation_per_frame *= 0.7
+	
+	# constant deceleration
+	if rotation_speed > 0:
+		rotation_speed = max(rotation_speed - decelerate * delta, 0)
 
 func increase_speed():
-	rotation_speed += 10  # Increase speed by 10 RPM (adjust as needed)
+	rotation_speed += 2  # increase speed by x RPM (adjust as needed)
